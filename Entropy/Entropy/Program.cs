3beefd33f0b5dcf0857863ee03a4d636fb7039e8
GMR Student <gmrstudent@outlook.com>
2022-01-12 19:56:35 -0800

    GReat

---
+++ Entropy/Entropy/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Entropy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string uncompressedLetters = Console.ReadLine();
+
+            int count = 1;
+            string compressedLetters = uncompressedLetters[0].ToString();
+            char lastLetter = compressedLetters[0];
+            for (int i = 1; i < uncompressedLetters.Length; i++)
+            {
+
+                if (uncompressedLetters[i] == lastLetter)
+                {
+                    count++;
+                }
+                else
+                {
+                    compressedLetters += count;
+                    count = 1;
+                    lastLetter = uncompressedLetters[i];
+                    compressedLetters += lastLetter;
+
+                }
+                if (i == uncompressedLetters.Length - 1)
+                {
+                    compressedLetters += count;
+                }
+            }
+
+
+
+            Console.WriteLine(compressedLetters);
+        }
+    }
+}
